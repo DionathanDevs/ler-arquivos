f6bd84ff9fdeb15736076303cdc06e5b3b061730
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-07-08 20:14:41 -0300

    Criando arquivo para funcões que trata o erro e exportando, para importar na index

---
+++ erros/trataErros.js
@@ -0,0 +1,12 @@
+function trataErro(erro){
+
+    if(erro.code === 'ENOENT'){
+        throw new Error('Arquivo não encontrado, verifique o caminho informado.')
+        //ou return 'Arquivo não encontrado' e chamar console.log na index
+    }else{
+        return 'Erro na aplicação'
+    }
+
+}
+//exportando função para ser usada como módulo ou seja trataErro(erro) em outro arquivo
+module.exports = trataErro;
