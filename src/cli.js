734224e3c3fe12893f55a45047ea557c26d7690a
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-07-10 22:11:18 -0300

    Adicionando lib externa Commander, separando function de processar arquivo

--- src/cli.js
+++ src/cli.js
@@ -1,34 +1,60 @@
 import fs from 'fs'
-// chamando módulo do FileSystem, para interagir com os arquivos acessar/alterar/ler
-//const fs = require('fs');
-//import para chamar o modulo trata erro
+import path from 'path'
 import trataErro from './erros/trataErros.js';
-
 import { montarSaidaArquivo } from './helpers.js';
-
-//const trataErros = require('./erros/trataErros.js')
 import { contaPalavras } from './lerArquivo.js';
-// const que armazena em array o caminho para o arquivo atual lerArquivos.js, e passamos um outro caminho na hora de executar o codigo no terminal, nesse caso utilizar ../arquivos/arquivo.txt
-const arquivoCaminho = process.argv;
+import { Command } from 'commander';
 
-//salvando o retorno do caminho digitado no terminal, no caso é o array do index 2
-const link = arquivoCaminho[2]
-//salvando o 3 elemento (endereco onde quero criar mneu arquivo txt)
-const endereco = arquivoCaminho[3]
+const program = new Command();
+
+program
+.version('0.0.1')
+.option('-t, --texto <string>', 'Caminho do texto a ser processado')
+.option('-d, --destino <string>', 'Caminho da pasta onde será salvo o arquivo de resultados.')
+.action((options) =>{
+const {texto, destino} = options;
+if(!texto || !destino){
+    console.error("Erro: Favor inserir caminho de origem e destino!");
+    program.help();
+    return;
+}
+
+const caminhoTexto = path.resolve(texto);
+const caminhoDestino = path.resolve(destino);
+
+try{
+    processaArquivo(caminhoTexto, caminhoDestino);
+    console.log("Texto processado com sucesso!");
+}catch(erro){
+    console.log("Ocorreu um erro no processamento", erro)
+}
+})
+
+program.parse()
+
+// const arquivoCaminho = process.argv;
+// //salvando o retorno do caminho digitado no terminal, no caso é o array do index 2
+// const link = arquivoCaminho[2]
+// //salvando o 3 elemento (endereco onde quero criar mneu arquivo txt)
+// const endereco = arquivoCaminho[3]
 
 //função do módulo filesystem, chamamos o módulo e a função de ler arquivo, passamos primeiro o caminho do arquivo, nesse caso a const link, depois disso codificacao dos caracteres e depois arrow function callback passamos os parametros err, data -> caso der err, e data o texto do arquivo
-fs.readFile(link, 'utf-8', (erro, texto) => {
+function processaArquivo(texto, destino){
+
+fs.readFile(texto, 'utf-8', (erro, texto) => {
     try {
         //mandando o erro para frente com throw, sem o throw eu nao consigo passar para o catch o erro captado pelo parametro da funcao readfile
         if (erro) throw erro
        const resultado = contaPalavras(texto);
-       criaESalvaArquivo(resultado,endereco );
+       criaESalvaArquivo(resultado, destino );
     } catch (erro) {
         //dessa forma exibe o stack trace -> por onde ele passou ate chegar no erro, informação mais completa do que somente apontar o que é o erro. 'trataErros(erro)'
         console.log(trataErro(erro))
     }
 })
-//Promise.all() para uma lista de arquivos a serem lidos
+
+}
+
 
 //mais simplificado
 
@@ -39,7 +65,7 @@ async function criaESalvaArquivo(listaPalavras, endereco){
 
     try {
         //retorna uma promessa, espera a resposta desse comando e segue o programa enquanto esse dado nao retorna
-
+        //Promise.all() para uma lista de arquivos a serem lidos
        await fs.promises.writeFile(arquivoNovo, textoPalavras)
         console.log("Arquivo criado!")
     }catch(err){
